f5b6df00bbc24d096676ebf8cc49d63c09ef5f47
RobinTorres7 <robinhood9607@gmail.com>
2022-11-24 20:34:59 -0600

    four commit

--- app/uti/middlewares/authValidation.js
+++ app/uti/middlewares/authValidation.js
@@ -1,8 +1,23 @@
-const authValidation = (req,res)=>{
+const jwt = require("jsonwebtoken");
+const config = process.env;
+
+const authValidation = (req,res,next)=>{
     console.log(req.body);
-/* if (){
-    res
-}; */
-next();
+    const token = req.body.token;
+    
+if (!token){
+    return res.status(403).send(" a token is required for authentication");
+};
+try {
+    
+    jwt.verify(token, config.SECRET_KEY);
+    console.log("verificando");
+
+}
+catch(err){
+    return res.status(401).send("invalid token");
+    console.log("fallo");
+
+}
 }
 module.exports = {authValidation}
