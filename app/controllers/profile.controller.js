626331e418d977d14f6bcd547d0230a3a4a1d2f5
RobinTorres7 <robinhood9607@gmail.com>
2022-11-30 18:36:18 -0600

    comit photo3

--- app/controllers/profile.controller.js
+++ app/controllers/profile.controller.js
@@ -1,8 +1,13 @@
 // const multer = require ('multer');
+const updatePhotoProfile = require('../uti/files/uploadfiles')
 
 const updatePhotoProfile = async (req,res)=>{
     console.log('api profile works');
     console.log(req.file);
+    const result = await uploadFromBuffer(req.file);
+    if(result){
+        console.log(result);
+    }
     // console.log(req.photo);
 }
 
