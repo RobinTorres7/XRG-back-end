02585552c7405253e5d953306ed377e0123d8881
RobinTorres7 <robinhood9607@gmail.com>
2022-12-13 18:51:32 -0600

    logingoogle

--- app/controllers/account.controller.js
+++ app/controllers/account.controller.js
@@ -1,7 +1,8 @@
 const Account = require('../models/accounts.model');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const profile = require('../models/profile.model');
 
 
 const getAccounts = (req, res) => {
@@ -9,6 +10,42 @@ const getAccounts = (req, res) => {
     res.send('newAccount api works!')
 
 }
+const googleAccount = async (req, res, next) => {
+    try {
+        const user = req.body;
+
+        const account = await Account.findOneAndUpdate(
+            { email: user.email },
+            { fullName: user.name }
+        );
+        if (account) {
+            profile.findOneAndUpdate(
+                { idAccount: account._id },
+                { photoProfile: user.photoUrl }
+            );
+            return (next);
+        }
+        const passwordHash = await bcrypt.hash(user.password, saltRounds)
+        const newAccount = await Account.create({
+            fullName: user.name,
+            email: user.email,
+            password: passwordHash,
+        })
+        if (newAccount) {
+            profile.create({
+                idAccount: newAccount._id,
+                photoProfile: user.photoUrl,
+            });
+        }
+        return next();
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            message: error.message,
+        });
+    }
+};
+
 const newAccount = async (req, res) => {
     try {
         bcrypt.hash(req.body.password, saltRounds,);
@@ -18,9 +55,9 @@ const newAccount = async (req, res) => {
             user.password = passwordHash
         }
         const result = await Account.create(user);
-        if(!result){
+        if (!result) {
             return res.status(400).json({
-                ok:false,
+                ok: false,
                 message: "No se puede crear cuenta"
             })
         }
@@ -45,4 +82,5 @@ const newAccount = async (req, res) => {
     }
 }
 
-module.exports = { newAccount, getAccounts };
+
+module.exports = { newAccount, getAccounts, googleAccount };
